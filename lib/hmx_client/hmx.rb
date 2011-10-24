f8a4ea2dc3c1442afa0248c671f7807586232480
Alan Moore <ukmoore@gmail.com>
2011-10-23 17:11:03 -0700

    Updating default response in API

--- lib/hmx_client/hmx.rb
+++ lib/hmx_client/hmx.rb
@@ -8,12 +8,16 @@ class Hmx
     response = RestClient.get url
     @context = JSON.parse(response.to_str)    
   end
+  def performRequest(prefix, function, command)
+    response = RestClient.post @urlapi+'/'+prefix, :function=>function, :params=>JSON.generate(command), :multipart=>true
+    return JSON.parse(response.to_str)
+  end
   def get(displayName)
     hash = Hash.new
     hash['context'] = @context
     hash['displayName'] = displayName
     response = performRequest('user', 'GETDATA', hash)
-    return response.to_str
+    return response['data']
   end
   def put(displayName, content)
     hash = Hash.new
@@ -21,10 +25,6 @@ class Hmx
     hash['displayName'] = displayName
     hash['content'] = content
     response = performRequest('user', 'PUTSIMPLEDATA', hash)
-    return response.to_str
+    return response['data']
   end
-  def performRequest(prefix, function, command)
-    response = RestClient.post @urlapi+'/'+prefix, :function=>function, :params=>JSON.generate(command), :multipart=>true
-    return response
-  end
- end
+end
